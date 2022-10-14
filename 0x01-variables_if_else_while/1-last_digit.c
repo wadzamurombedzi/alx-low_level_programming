c60b24a8637944fd56ee08c73697ee8a84cdf0aa
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-14 16:29:49 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/*betty style doc for function main goes there */
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
+
+	int m;
+
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d", n);
-		printf(" is positive\n");
-	}
-	if (n == 0)
-	{
-		printf("%d", n);
-		printf(" is zero\n");
-	}
-	if (n < 0)
-	{
-		printf("%d", n);
-		printf(" is negative\n");
-	}
+	n = rand() -RAND_MAX / 2;
+	/* your code goes there */
+	m = n % 10;
+	if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	if (m == 0)
+		printf("Last difit of %d is %d and is 0\n", n, m);
+	if (m <6 && m != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
 }
