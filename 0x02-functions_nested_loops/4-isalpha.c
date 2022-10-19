4a72879b7fe2ba1dabf9848b91e790b74409f47f
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-18 17:22:50 -0700

    Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  *_isalpha - checks for alphabets
  * @c: character to be checked
  *
- * Return: 1 if character is a letter, 0 otherwise
+ * Return: 1 if character is a letter,0 otherwise
  */
-int_isalpha(int char)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (10);
