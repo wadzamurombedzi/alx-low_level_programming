93e93f5af2bc74af939acc8c5ee034304ef94cf8
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-19 16:51:26 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints from any integer to 98
+ * @n: interger input
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	printf("98\n");
+}
+
+
+
+
