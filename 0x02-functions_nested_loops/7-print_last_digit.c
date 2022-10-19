798588f50481de3a4c62ad4e2b9cb518e3bcc6ad
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-19 10:46:48 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of integer input
+ * @n: integer to modify
+ *
+ * Return: 1
+ */
+int print_last_digit(int n)
+{
+	int 1, p;
+
+	1 = n % 10;
+	if (1 < 0)
+		1 = -1;
+	p = '0' + 1;
+	_putchar(p);
+	return (1);
+}
