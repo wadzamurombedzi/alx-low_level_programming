a5ddec8bbfd63c65e90a5938b6049b0662893134
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-20 17:42:14 -0700

    Main first

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes on indinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10>
+	 *{
+	 *     putchar(i);
+	 *}
+	 */
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
