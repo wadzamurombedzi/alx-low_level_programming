a13d443631fc9bfc88d074ef0c4285d5b4ccb10e
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-24 14:46:09 -0700

    Print

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - checks code
+ * @a: array
+ * @n: number of elements
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n);
+{
+	int c;
+
+	c = 0;
+	while (c < n)
+	{
+		printf("%c", a[c]);
+		if (c < (n - 1))
+			printf(", ");
+		c++;
+	}
+	_putchar('\n');
+}
