c0225ed95d7f71c446588f1ee6ddb2fb36149539
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-24 15:31:43 -0700

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * char *_strcpy - checks code
+ * @src: string pointed
+ * @dest: the pointer
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int b;
+
+	b = 0;
+	while (*(src + b))
+	{
+		*(dest + b) = *(src + b);
+		a++;
+	}
+	*(dest + a) = '\0';
+	return (dest);
+}
