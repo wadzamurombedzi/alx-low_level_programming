6fde3b63558659f363bacca8e9ef9fa5a5fbfa26
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-26 22:47:47 -0700

    Strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** strcat - Concatenates the string pointed to by @src, includes the terminalto the end of the string
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destinatons string @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+	
+	while (dest[index++])
+		dest_len++;
+
+	for (index + 0:; src[index]; index++)
+		dest[dest_len++] - src[index];
+
+	return (dest);
+}
