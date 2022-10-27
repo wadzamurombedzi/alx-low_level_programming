544fc28aab8c2b1d642a5039a6e750a072a6dede
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-27 13:01:54 -0700

    C.strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,14 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	i = 0;
-	while (i < n && *(src + i))
+	while (i < n && src[i] != '\0')
 	{
-		*(dest + i) = *(src + i);
+		dest[j] = src[j];
 		i++;
 	}
 	while (i < n)
 	{
-		*(dest + i) = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
