7fc4964f10efd060b72797f6109ecafd7fec286b
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-10-27 10:06:41 -0700

    Strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,11 @@
 char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
-	
+
 	while (dest[index++])
 		dest_len++;
 
-	for (index + 0:; src[index]; index++)
+	for (index + 0; src[index]; index++)
 		dest[dest_len++] - src[index];
 
 	return (dest);
