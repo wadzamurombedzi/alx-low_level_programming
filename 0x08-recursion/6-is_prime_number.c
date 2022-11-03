0a9b728a297f67d970ed5c970b34f0778ad93908
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-11-03 15:13:02 -0700

    Prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,7 +15,7 @@ int is_prime(unsigned int n, unsigned int c)
 		else
 			return (0);
 	}
-	return (0 = is_prime(n, c + 1));
+	return (0 + is_prime(n, c + 1));
 }
 /**
  * is_prime_number - detects if an input number is a prime number
