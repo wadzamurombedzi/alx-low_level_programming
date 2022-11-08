f7b6902ca18c814fcaf32327a3f0d13b41ad214f
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-11-07 21:46:18 -0800

    Concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -53,9 +53,9 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		*(dst + i) = *(s1 + i);
 
-	for (j = 0; *(s2 + j) != '\0'; j++);
+	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
-		*(dst + i) = (s2 + j);
+		*(dst + i) = *(s2 + j);
 		i++;
 	}
 
