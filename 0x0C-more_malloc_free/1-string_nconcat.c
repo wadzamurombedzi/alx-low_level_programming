db03f0c995615a0d8bbdcd5f2a76b40789f110a1
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-11-10 13:02:16 -0800

    Concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,14 +20,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[size 2] != '\0')
+	while (s1[size 1] != '\0')
+	{
+		size1++;
+	}
+
+	while (s2[size2] != '\0')
 	{
 		size2++;
 	}
 
 	if (n > size2)
 		n = size2;
-	p = malloc(size1 + n + 1) * sizeof(char);
+	p = malloc((size1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
 		return (0);
@@ -43,4 +48,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p[i] = '\0';
 
 	return (p);
-}
+iiiiiiiii}
