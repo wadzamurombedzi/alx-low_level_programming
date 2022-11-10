4ee94c4032af3158f8a4c7d4b5b02b2cdf304a5e
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-11-10 15:02:08 -0800

    Realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -37,7 +37,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (0);
 	}
-	for (i = 0; i < (old_school || i < new_size); i++)
+	for (i = 0; i < (old_size || i < new_size); i++)
 	{
 		*(relloc + i) = clone[i];
 	}
